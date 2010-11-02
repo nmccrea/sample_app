211f543456881d0c21ad203458ed543a8b24f8c3
Nick McCrea <nmccrea@Applesaws.local>
2010-11-02 15:23:55 -0500

    Added a Sessions controller and a signin view

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,12 +21,17 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => 'Help')
   end
-  
+
   it "should have a signup page at '/signup'" do
     get '/signup'
     response.should have_selector('title', :content => 'Sign up')
   end
-  
+
+  it "should have a signin page at '/signin'" do
+    get '/signin'
+    response.should have_selector('title', :content => 'Sign in')
+  end
+
   it "should have the right links on the layout" do
     visit root_path
     response.should have_selector('title', :content => 'Home')
