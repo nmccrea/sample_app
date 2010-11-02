a170b0dfc62b7b82de0b99f7486248aa557c832f
Nick McCrea <nmccrea@Applesaws.local>
2010-11-02 10:18:01 -0500

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Nick McCrea"
+  user.email                 "nickmccrea@gmail.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
